2d963a3b8ccfe9b04dd5d499afe0c4fb17e66aa0
Alireza Nori <alireza.nr1378@gmail.com>
2021-06-19 10:20:07 +0430

    Create mapper interface.

---
+++ app/src/main/java/dan/nr/myapplication/util/EntityMapper.kt
@@ -0,0 +1,7 @@
+package dan.nr.myapplication.util
+
+interface EntityMapper<Entity, DomainModel>
+{
+    fun mapFromEntity(entity: Entity): DomainModel
+    fun mapToEntity(domainModel: DomainModel): Entity
+}
