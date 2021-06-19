42fc60e098937a263cab73b45dd8fc50a74dd00f
Alireza Nori <alireza.nr1378@gmail.com>
2021-06-19 12:53:24 +0430

    Create remote data source to providing different api interfaces.

---
+++ app/src/main/java/dan/nr/myapplication/network/RemoteDataSource.kt
@@ -0,0 +1,35 @@
+package dan.nr.myapplication.network
+
+import androidx.viewbinding.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import javax.inject.Inject
+
+
+class RemoteDataSource @Inject constructor()
+{
+    @Inject
+    lateinit var retrofit: Retrofit.Builder
+
+    fun <Api> buildApi(api: Class<Api>, authToken: String? = null): Api
+    {
+        return retrofit
+                .client(OkHttpClient.Builder()
+                                .addInterceptor { chain ->
+                                    chain.proceed(chain.request().newBuilder().also {
+                                        it.addHeader("Authorization", "Bearer $authToken")
+                                    }.build())
+
+                                }.also { client ->
+                            if (BuildConfig.DEBUG)
+                            {
+                                val logging = HttpLoggingInterceptor()
+                                logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+                                client.addInterceptor(logging)
+                            }
+                        }.build())
+                .build()
+                .create(api)
+    }
+}
