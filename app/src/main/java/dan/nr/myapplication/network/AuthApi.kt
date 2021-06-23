9d247a9d32dd95f5e0dcbc42a5e29e44c30b814f
Alireza Nori <alireza.nr1378@gmail.com>
2021-06-24 02:31:19 +0430

    Refactor auth apis.

--- app/src/main/java/dan/nr/myapplication/network/AuthApi.kt
+++ app/src/main/java/dan/nr/myapplication/network/AuthApi.kt
@@ -1,23 +1,23 @@
 package dan.nr.myapplication.network
 
-import dan.nr.myapplication.model.auth.AuthModel
-import retrofit2.Response
+import dan.nr.myapplication.model.auth.AuthResponse
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
 import retrofit2.http.POST
 
-interface AuthenticationApi
+interface AuthApi
 {
+    @FormUrlEncoded
     @POST("auth/signup")
     suspend fun signup(@Field("name") name: String,
                        @Field("email") email: String,
                        @Field("password") password: String,
-                       @Field("password_confirmation") password_confirmation: String): Response<AuthModel>
+                       @Field("password_confirmation") password_confirmation: String): AuthResponse
 
-
+    @FormUrlEncoded
     @POST("auth/login")
     suspend fun login(@Field("email") email: String,
-                      @Field("password") password: String): Response<AuthModel>
+                      @Field("password") password: String): AuthResponse
 
-    @POST("logout")
-    suspend fun logout(): Response<AuthModel>
 }
